b8b9405e1cae16c42c4b4210112656c7cc232a72
akcio23 <dev.akcio@gmail.com>
2025-03-27 22:06:15 -0300

    feat: create component loggend user

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,21 +2,28 @@
 import React from 'react'
 import serializeruser from '../_serializer/serializeruser'
 import { ButtonHomeRedirect } from '../_components/Buttons'
+import DefaultAllpages from '../_components/DefaultAllpages'
 
 const Home = () => {
   const data = JSON.parse(sessionStorage.getItem('user'))
-  const user = serializeruser(data)
-  console.log(data)
+  const customer = serializeruser(data)
+  localStorage.setItem('customer', JSON.stringify(customer))
 
   return (
-    <div>
-      {!user?._id ? (
-        <div>
-          <p>Não logado</p>
-          <ButtonHomeRedirect />
+    <div className="h-screen flex justify-center items-center">
+      {!customer?._id ? (
+        <div className="bg-cover bg-center min-h-screen flex flex-col justify-center items-center">
+          <div className="flex flex-col gap-10  h-96 justify-center items-center">
+            <p className="text-white text-center font-bold">
+              Faça login para continuar!
+            </p>
+            <div className="flex flex-col gap-5 ">
+              <ButtonHomeRedirect />
+            </div>
+          </div>
         </div>
       ) : (
-        <div>page</div>
+        <DefaultAllpages customer={customer} />
       )}
     </div>
   )
